0b1948e345d72e6ee205eb17ceee09702d918ad2
sueyoungshim <sueyoungshim@berkeley.edu>
2023-07-26 14:19:04 -0700

    created skeleton code for home and create party

---
+++ src/components/CreateParty.js
@@ -0,0 +1,16 @@
+function CreateParty() {
+    return (
+        <div>
+            Party Name:
+            <input type="text"></input>
+            <br></br>
+            Location:
+            <input type="text"></input>
+            <br></br>
+            Time:
+            <input type="text"></input>
+        </div>
+    );
+}
+
+export default CreateParty;
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import Button from 'react-bootstrap/Button';
+
+console.log('home is running');
+
+function home() {
+    return (
+        <div>
+            <h3>Party Bears</h3>
+            <div>
+                <Button href='/CreateParty'> Create a New Party </Button>
+            </div>
+            <div>
+                List of Parties here / displayed in card format
+            </div>
+        </div>
+    );
+}
+
+export default home;
