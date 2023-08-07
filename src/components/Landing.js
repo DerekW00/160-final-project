fe7d71339e0d2f6591911595930b22d6a6966496
sueyoungshim <sueyoungshim@berkeley.edu>
2023-08-07 12:20:27 -0700

    added landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,28 @@
+import { Button, Stack, Center } from '@chakra-ui/react'
+import { Container } from 'react-bootstrap';
+
+
 function Landing() {
     return (
         <div>
-            <img src="https://github.com/DerekW00/160-final-project/blob/main/src/images/logo.png" alt='logo'></img>
+            <div style={ {height: '350px'} }></div>
+            <Center>
+                <img src="logo.png" width="300" alt="logo" />
+            </Center>
+            <div style={ {height: '200px'} }></div>
+            <Center>
+                <Stack direction='row' spacing={4} centerContent>
+                    <Button colorScheme='gray' variant='solid'>
+                        Sign In
+                    </Button>
+                    <Button colorScheme='white' variant='outline'>
+                        Sign Up
+                    </Button>
+                </Stack>
+            </Center>
         </div>
+            
+            
     );
 }
 
