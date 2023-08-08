3af2ecb16901ad7a651ce4b2bffbc46f8f709523
sueyoungshim <sueyoungshim@berkeley.edu>
2023-08-07 22:41:52 -0700

    profile skeleton

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,50 +2,35 @@ import { getAuth } from "firebase/auth";
 
 const auth = getAuth();
 const user = auth.currentUser;
-if (user !== null) {
-  // The user object has basic properties such as display name, email, etc.
-  const displayName = user.displayName;
-  const email = user.email;
-  const photoURL = user.photoURL;
-  const emailVerified = user.emailVerified;
-
-  // The user's ID, unique to the Firebase project. Do NOT use
-  // this value to authenticate with your backend server, if
-  // you have one. Use User.getToken() instead.
-  const uid = user.uid;
-
-  console.log(email, displayName, photoURL, emailVerified, uid);
-}
-
 
 function Profile() {
     const auth = getAuth();
     const user = auth.currentUser;
-    if (user !== null) {
+    if (!user) {
         // The user object has basic properties such as display name, email, etc.
-        const displayName = user.displayName;
-        const email = user.email;
-        const photoURL = user.photoURL;
-        const emailVerified = user.emailVerified;
-
-        // The user's ID, unique to the Firebase project. Do NOT use
-        // this value to authenticate with your backend server, if
-        // you have one. Use User.getToken() instead.
-        const uid = user.uid;
-        console.log(user);
-        console.log(email, displayName, photoURL, emailVerified, uid);
+        return <div> You are not logged in. </div>
     }
+
+    const displayName = user.displayName;
+    const email = user.email;
+    const photoURL = user.photoURL;
+    const emailVerified = user.emailVerified;
+
+    // The user's ID, unique to the Firebase project. Do NOT use
+    // this value to authenticate with your backend server, if
+    // you have one. Use User.getToken() instead.
+    const uid = user.uid;
+    console.log(user);
+    console.log(email, displayName, photoURL, emailVerified, uid);
     return (
       <div className='center'> 
-        {/* <div className='profile'>
+        <div className='profile'>
           <h1>Profile</h1>
-          <p><strong>Email: </strong>{`${email}`}</p>
-          <p>
-            <strong>Email verified: </strong>
-            {`${currentUser?.emailVerified}`}
-          </p>
+          <p><strong>Name: </strong>{displayName}</p>
+          <p><strong>Email: </strong>{email}</p>
+          
           <span>Sign Out</span>
-        </div> */}
+        </div>
       </div>
     )
   }
