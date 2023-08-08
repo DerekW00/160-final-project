1712039ca98c1274f916614f79e750d222e7d089
sueyoungshim <sueyoungshim@berkeley.edu>
2023-08-07 18:56:48 -0700

    adding Context API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import CreateEvent from './components/CreateEvent';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {useState} from 'react'
+import {AuthProvider} from './AuthContext'
 import Home from './components/Home';
 import Calendar from './components/Calendar';
 import SignIn from './components/SignIn';
@@ -7,17 +9,20 @@ import Landing from './components/Landing';
 import SignUp from './components/SignUp';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
   return (
     <div>
       <Router>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/CreateEvent' element={<CreateEvent />} />
-          <Route path='/Calendar' element={<Calendar />} />
-          <Route path='/SignIn' element={<SignIn />} />
-          <Route path='/Landing' element={<Landing />} />
-          <Route path='/SignUp' element={<SignUp />} />
-        </Routes>
+        <AuthProvider value={{currentUser}}>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/CreateEvent' element={<CreateEvent />} />
+            <Route path='/Calendar' element={<Calendar />} />
+            <Route path='/SignIn' element={<SignIn />} />
+            <Route path='/Landing' element={<Landing />} />
+            <Route path='/SignUp' element={<SignUp />} />
+          </Routes>
+        </AuthProvider>
       </Router>
     </div>
   );
